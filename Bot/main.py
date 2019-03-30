b2406d491ba464d4dc554fa80540c27a24d4ef2e
tpennypacker <trevp@seas.upenn.edu>
2019-03-30 16:47:15 -0500

    parsed a few message types

--- Bot/main.py
+++ Bot/main.py
@@ -1,6 +1,9 @@
 # Based on the Pokemon Showdown protocol, found here:
 # https://github.com/Zarel/Pokemon-Showdown/blob/master/PROTOCOL.md
 
+# Inspired by Synedh's bot, found here: 
+# https://www.smogon.com/forums/threads/showdown-battle-bot-a-socket-battle-bot-for-showdown.3644033/
+
 import asyncio
 import websockets
 import requests
@@ -15,7 +18,11 @@ password = "ascii"
 async def parse_response(ws, msg):   
     msg_arr = msg.split('|')
 
-    if (msg_arr[1] == "challstr"):
+    if(len(msg_arr) < 2):
+        print(msg)
+
+    # log in
+    elif (msg_arr[1] == "challstr"):
         print("Challstr received")
         ID = msg_arr[2]
         challstr = msg_arr[3]
@@ -34,15 +41,42 @@ async def parse_response(ws, msg):
     elif (msg_arr[1] == 'updateuser' and "Guest" in msg_arr[2]):
         print("Currently registered as guest")
 
+    # login successful!
     elif (msg_arr[1] == 'updateuser' and username in msg_arr[2]):
-        print("Congrats, you've logged in! Searching for game now.")
-        randbat_string = "gen7randombattle"#parsed_formats[8]
-        search_msg = "|/search " + randbat_string
-        await ws.send(search_msg)
+        print("Congrats, you've logged in! Typing /ionext now.")
+        to_send = "|/ionext"
+        await ws.send(to_send)
+        # randbat_string = "gen7randombattle"#parsed_formats[8]
+        # search_msg = "|/search " + randbat_string
+        # await ws.send(search_msg)
+        # await ws.send("|/help")
+        # await ws.send("|/help msg")
+
+    # on receiving a message in battle chat
+    elif ('|c|' in msg):
+        print("battle chat message incoming")
+        print(msg)
+        #await ws.send("|/msg GenOne, I am a robot and GenOne sucks at pokemon")
+
+    # upon receiving a private message
+    elif ('|pm|' in msg):
+        print("private message incoming")
+        print(msg)
+
+    # when a new battle starts
+    elif ('battle' in msg_arr[0] and msg_arr[1] == 'init'):
+        to_send = "|/ionext"
+        await ws.send(to_send)        
+        battle_tag = msg_arr[0].split('>')[1].split('\n')[0]
+        print("Battle started: " + battle_tag)
 
     else:
         print("Unknown message type incominggggggg")
         print(msg)
+        if ('>' in msg_arr[0]):
+            battle_tag = msg_arr[0].split('>')[1].split('\n')[0]
+            to_send = battle_tag + "|hello!"
+            print("SENDING: " + to_send)
 
 
 
