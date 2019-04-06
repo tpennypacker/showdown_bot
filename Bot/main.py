d500bf2e32c3c602edaa18b1bf263b9b27028148
tpennypacker <trevp@seas.upenn.edu>
2019-04-06 14:29:18 -0500

    automatically chooses moves

--- Bot/main.py
+++ Bot/main.py
@@ -1,110 +1,55 @@
-# Based on the Pokemon Showdown protocol, found here:
-# https://github.com/Zarel/Pokemon-Showdown/blob/master/PROTOCOL.md
-
-# Inspired by Synedh's bot, found here: 
-# https://www.smogon.com/forums/threads/showdown-battle-bot-a-socket-battle-bot-for-showdown.3644033/
-
 import asyncio
 import websockets
 import requests
 import json
 import os
+from string import printable
 
-os.system('clear')
-username = "Ask Key"
-password = "ascii"
+import bot_settings
+import funcs
 
+async def parse_response(ws, msg):
+	msg_arr = msg.split('|')
 
-async def parse_response(ws, msg):   
-    msg_arr = msg.split('|')
+	if (len(msg_arr) < 3):
+		pass
 
-    if(len(msg_arr) < 2):
-        print(msg)
+	# triggers at the start of each turn
+	elif (msg_arr[1] == "request" and len(msg_arr[2]) > 0):
+		battletag = msg_arr[0][1:].split('\n')[0]
+		await funcs.choose_moves(ws, msg_arr[2], battletag)
 
-    # log in
-    elif (msg_arr[1] == "challstr"):
-        print("Challstr received")
-        ID = msg_arr[2]
-        challstr = msg_arr[3]
-        response = requests.post("https://play.pokemonshowdown.com/action.php?",
-                         data={
-                            'act': 'login',
-                            'name': username,
-                            'pass': password,
-                            'challstr': ID + "%7C" + challstr
-                         })
-        jdata = json.loads(response.text[1:]) # load response into json array. [1:] is to get rid of the first character
-        login_msg = "|/trn " + username + ",0," + jdata['assertion']
-        await ws.send(login_msg)
+	# check for short messages
+	elif (len(msg_arr) < 4):
+		pass
 
-    # if haven't logged in yet
-    elif (msg_arr[1] == 'updateuser' and "Guest" in msg_arr[2]):
-        print("Currently registered as guest")
+	# login action
+	elif (msg_arr[1] == "challstr"):
+		await funcs.log_in(ws, msg_arr)
 
-    # login successful!
-    elif (msg_arr[1] == 'updateuser' and username in msg_arr[2]):
-        print("Congrats, you've logged in! Typing /ionext now.")
-        to_send = "|/ionext"
-        await ws.send(to_send)
-        # randbat_string = "gen7randombattle"#parsed_formats[8]
-        # search_msg = "|/search " + randbat_string
-        # await ws.send(search_msg)
-        # await ws.send("|/help")
-        # await ws.send("|/help msg")
+	# triggers when user first logs in
+	elif (msg_arr[1] == 'updateuser' and msg_arr[2].lower() == bot_settings.username.lower()):
+		await funcs.startup_ops(ws)
 
-    # on receiving a message in battle chat
-    elif ('|c|' in msg):
-        print("battle chat message incoming")
-        print(msg)
-        #await ws.send("|/msg GenOne, I am a robot and GenOne sucks at pokemon")
+	# triggers when a battle starts
+	elif ('>battle' in msg_arr[0] and msg_arr[1] == 'player' and msg_arr[3].lower() == bot_settings.username.lower()):
+		battletag = msg_arr[0][1:].split('\n')[0]
+		await funcs.on_battle_start(ws, battletag)
 
-    # upon receiving a private message
-    elif ('|pm|' in msg):
-        print("private message incoming")
-        print(msg)
-
-    # when a new battle starts
-    elif ('battle' in msg_arr[0] and msg_arr[1] == 'init'):
-        to_send = "|/ionext"
-        await ws.send(to_send)        
-        battle_tag = msg_arr[0].split('>')[1].split('\n')[0]
-        print("Battle started: " + battle_tag)
-
-    else:
-        print("Unknown message type incominggggggg")
-        print(msg)
-        if ('>' in msg_arr[0]):
-            battle_tag = msg_arr[0].split('>')[1].split('\n')[0]
-            to_send = battle_tag + "|hello!"
-            print("SENDING: " + to_send)
-
-
+	# triggers when a battle ends
+	elif ('>battle' in msg_arr[0] and msg_arr[2] == 'win'):
+		battletag = msg_arr[0][1:].split('\n')[0]
+		await funcs.on_battle_end(ws, battletag)
 
 
 
 async def connect_to_ps():
     async with websockets.connect("ws://sim.smogon.com:8000/showdown/websocket") as ws:
         while(True):
-            print("_____________________________")
+            #print("_____________________________")
             msg = await ws.recv()
-            #print(msg)
             await parse_response(ws, msg)
-            print("_____________________________")
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+#os.system('cls') # windows
+os.system('clear') # mac
 asyncio.get_event_loop().run_until_complete(connect_to_ps())
